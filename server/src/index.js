9e3f16e59cb5c2196e23021586aec8ce6616c8aa
Rafiq Lightwala <rafiq.lightwala@gmail.com>
2023-06-15 00:52:04 -0600

    Set up app, logging and basic connections: - Set up the index.js page for connecting to MongoDB with Mongoose - Set up app.js with morgan and winston logging - Set up config folder for accessing environment variables - set up winston and morgan logging files - updated gitignore

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,38 @@
-const express = require('express');
+const mongoose = require('mongoose');
+const app = require('./app');
+const config = require('./config/config');
+const logger = require('./config/logger');
 
-const port = 3200;
+let server;
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  logger.info('Connected to MongoDB');
+  server = app.listen(config.port, () => {
+    logger.info(`Listening to port ${config.port}`);
+  });
+});
 
-const app = express();
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
 
-app.listen(port, () => {
-  console.log(`***Server running on ${port}***`);
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
 });
