48a7c69f713a187d4a73cd4456f25ac0ffca17e4
Rafiq Lightwala <rafiq.lightwala@gmail.com>
2023-06-15 01:30:41 -0600

    Removed some code for tomorrow's work which was making the server crash. Now server is starting as intended.

--- server/src/app.js
+++ server/src/app.js
@@ -37,25 +37,17 @@ app.use(cors());
 app.options('*', cors());
 
 // jwt authentication
-app.use(passport.initialize());
+//app.use(passport.initialize());
 
 // limit repeated failed requests to auth endpoints
 if (config.env === 'production') {
     app.use('/v1/auth', authLimiter);
 }
 
-// v1 api routes
-app.use('/v1', routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
     next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
-
-// convert error to ApiError, if needed
-app.use(errorConverter);
-
-// handle error
-app.use(errorHandler);
 
 module.exports = app;
