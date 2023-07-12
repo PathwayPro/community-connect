d5f6b28abdffc8b7c2d076b668dbf127d7079d64
Kristina Protsiak <68956204+Burning-Shifu@users.noreply.github.com>
2023-07-12 23:15:44 +0300

    uptated styles and pics of partners carousel (#16)
    * uptated styles and pics of partners carousel
    
    * added swiping to carousel
    
    * add swiper: in process
    
    * main page: changed carousel to slick
    
    * main page: fixed comments to PR
    
    * package.json: fixed duplicated line
    
    ---------
    
    Co-authored-by: Shifu <kris.deniso@mail.ru>

--- client/src/pages/Main/Partners/Partners.tsx
+++ client/src/pages/Main/Partners/Partners.tsx
@@ -1,33 +1,61 @@
 import { FC } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
 
+import CarouselArrow from '../../../common/components/CarouselArrow/CarouselArrow';
+import stylesCarouselDots from '../../../common/components/CarouselDots/CarouselDots.module.scss';
 import Heading from '../../../common/components/Heading/Heading';
-import Carousel from '../../../components/Carousel/Carousel';
-import CarouselSlide from '../../../components/Carousel/CarouselSlide/CarouselSlide';
 import SectionMain from '../../../components/SectionMain/SectionMain';
 
 import styles from './Partners.module.scss';
 
-import partner1Image from '../../../images/Partners/partner-1.png';
-import partner2Image from '../../../images/Partners/partner-2.png';
-import partner3Image from '../../../images/Partners/partner-3.png';
+import partner9Image from '../../../images/Partners/AppHarvest.jpg';
+import partner7Image from '../../../images/Partners/BettyLabs.jpg';
+import partner1Image from '../../../images/Partners/Blind.jpg';
+import partner10Image from '../../../images/Partners/Cohesity.jpg';
+import partner5Image from '../../../images/Partners/Coinbase.jpg';
+import partner2Image from '../../../images/Partners/Helbiz.jpg';
+import partner6Image from '../../../images/Partners/MasterClass.jpg';
+import partner8Image from '../../../images/Partners/Replica.jpg';
+import partner4Image from '../../../images/Partners/Sonos.jpg';
+import partner3Image from '../../../images/Partners/Upstart.jpg';
 
-const partnersList = {
-  slide1: [partner1Image, partner2Image, partner3Image],
-  slide2: [partner1Image, partner2Image, partner3Image],
-  slide3: [partner1Image, partner2Image, partner3Image],
-};
+const partnersImages = [
+  partner1Image,
+  partner2Image,
+  partner3Image,
+  partner4Image,
+  partner5Image,
+  partner6Image,
+  partner7Image,
+  partner8Image,
+  partner9Image,
+  partner10Image,
+];
 
 const Partners: FC = () => {
+  const settings = {
+    className: `${styles.slider}`,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    dotsClass: `${stylesCarouselDots.carouselDots}`,
+    nextArrow: <CarouselArrow />,
+    prevArrow: <CarouselArrow />,
+  };
   return (
     <SectionMain>
       <Heading tagType="h2" className={styles.heading}>
         Our partners
       </Heading>
-      <Carousel>
-        <CarouselSlide slidesList={partnersList.slide1} />
-        <CarouselSlide slidesList={partnersList.slide2} />
-        <CarouselSlide slidesList={partnersList.slide3} />
-      </Carousel>
+      <Slider {...settings}>
+        {partnersImages.map((image, index) => (
+          <div className={styles.slide} key={Math.random()}>
+            <img src={image} alt={`partner-${index + 1}`} />
+          </div>
+        ))}
+      </Slider>
     </SectionMain>
   );
 };
