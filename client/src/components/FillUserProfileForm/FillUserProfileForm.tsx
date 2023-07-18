d15923861d00f06327c4023126563f4e3d7b7073
Shifu <kris.deniso@mail.ru>
2023-07-17 22:27:46 -0600

    FillUserProfileForm: add progress bar

--- client/src/components/FillUserProfileForm/FillUserProfileForm.tsx
+++ client/src/components/FillUserProfileForm/FillUserProfileForm.tsx
@@ -4,8 +4,11 @@ import { useForm, UseFormRegister } from 'react-hook-form';
 import { useAppDispatch } from '../../app/hooks';
 import { closeModal } from '../../app/slices/modalSlice';
 import Button from '../../common/components/Button/Button';
+import Heading from '../../common/components/Heading/Heading';
 
+import styles from './FillUserProfileForm.module.scss';
 import { IFormInput } from './formInputInterface';
+import ProgressBar from './ProgressBar/ProgressBar';
 import Step1 from './Step1';
 import Step2 from './Step2';
 import Step3 from './Step3';
@@ -28,17 +31,29 @@ const FillUserProfileForm: FC = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {step === 1 && <Step1 register={register} data={watchedFields} />}
-      {step === 2 && <Step2 register={register} data={watchedFields} />}
-      {step === 3 && <Step3 register={register} data={watchedFields} />}
-      <Button
-        label={step === 1 ? 'Skip' : 'Back'}
-        onClick={step === 1 ? () => dispatch(closeModal()) : () => setStep((prevStep) => prevStep - 1)}
-      />
-      <Button
-        label={step === 3 ? 'Finish' : 'Next'}
-        onClick={step === 3 ? handleSubmit(onSubmit) : () => setStep((prevStep) => prevStep + 1)}
-      />
+      <Heading tagType="h4" className={styles.title}>
+        {step === 1 && 'Welcome back Niloofar!'}
+        {step === 2 && 'Basic Information'}
+        {step === 3 && 'Your Goal'}
+      </Heading>
+      <ProgressBar step={step} />
+      <div className={styles.content}>
+        {step === 1 && <Step1 register={register} data={watchedFields} />}
+        {step === 2 && <Step2 register={register} data={watchedFields} />}
+        {step === 3 && <Step3 register={register} data={watchedFields} />}
+      </div>
+      <div className={styles.btnWrapper}>
+        <Button
+          size="small"
+          label={step === 1 ? 'Skip' : 'Back'}
+          onClick={step === 1 ? () => dispatch(closeModal()) : () => setStep((prevStep) => prevStep - 1)}
+        />
+        <Button
+          size="small"
+          label={step === 3 ? 'Finish' : 'Next'}
+          onClick={step === 3 ? handleSubmit(onSubmit) : () => setStep((prevStep) => prevStep + 1)}
+        />
+      </div>
     </form>
   );
 };
---
+++ client/src/components/FillUserProfileForm/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import styles from './ProgressBar.module.scss';
+
+type ProgressBarProps = {
+  step: number;
+};
+
+const stepsLabels = ["Basic Information", "Social Media", "Resume/CV", "Your Goal"];
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ step }) => {
+  return (
+
+    <div className={styles.progressBar}>
+      {stepsLabels.map((label, index) => {
+        const currentStep = index + 1;
+        const stepClass =
+          currentStep === step ? styles.active
+          : currentStep < step ? styles.finished
+          : styles.inactive;
+
+        return (
+          <div key={label} className={`${styles.block} ${stepClass}`}>
+            <div className={styles.dot} />
+            <span className={styles.label}>{label}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ProgressBar;
