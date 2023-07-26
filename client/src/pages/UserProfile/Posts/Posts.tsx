b0fd182e05a48619f13ad4e84f6230b6f77af7da
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-07-26 16:01:59 -0600

    add blank post component

---
+++ client/src/pages/UserProfile/Posts/Posts.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+import styles from './Posts.module.scss';
+
+const Posts: FC = () => {
+
+  return (
+    <div className={styles.posts}>
+      Posts
+    </div>
+  );
+};
+
+export default Posts;
