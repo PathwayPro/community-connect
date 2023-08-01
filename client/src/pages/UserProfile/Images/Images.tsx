48ba777e725cc7d06b0896f1185d6e59f7500c07
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-08-01 12:35:06 -0600

    adding a default image for user profile

--- client/src/pages/UserProfile/Images/Images.tsx
+++ client/src/pages/UserProfile/Images/Images.tsx
@@ -4,16 +4,30 @@ import VectorEditBtn from '../../../common/components/Vector/VectorEditBtn';
 
 import styles from './Images.module.scss';
 
+import defaultProfileImage from '../../../images/Main/defaultProfileImg.png';
+
 const Images: FC = () => {
   const openModal = () => console.log('click');
 
+  const isBGImageAvailable = true;
+  const isProfileImageAvailable = false;
+  const source = !isProfileImageAvailable
+    ? defaultProfileImage
+    : '';
+
   return (
-    <div className={styles.background}>
-        <img className={styles.image} src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png' />
-        <VectorEditBtn
-            position={'bottom'}
-            onClick={openModal}
+    <div className={styles.container}>
+        {isBGImageAvailable && (
+        <img
+          className={styles.backgroundImage}
+          src='https://cutewallpaper.org/21x/w02yrwp96/Colorful-Flowers-cover-covers-for-Facebook-pages-Flower-.jpg'
         />
+      )}
+      <img className={styles.image} src={source} alt="Your Image" />
+      <VectorEditBtn
+        position={'bottom'}
+        onClick={openModal}
+      />
     </div>
   );
 };
