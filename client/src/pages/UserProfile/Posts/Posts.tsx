fdb3ad8bd5962d1621b228b1ffc0969b4c942206
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-08-01 22:45:19 -0600

    add posts related component

--- client/src/pages/UserProfile/Posts/Posts.tsx
+++ client/src/pages/UserProfile/Posts/Posts.tsx
@@ -4,14 +4,61 @@ import Scroll from '../../../common/components/Scroll/Scroll';
 
 import AddPost from './AddPost/AddPost';
 import styles from './Posts.module.scss';
+import ShowPost, { ShowPostProps } from './ShowPost/ShowPost';
+
+const posts: ShowPostProps[] = [
+  {
+    imgPath: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+    name: 'Clark Mante',
+    position: 'Technician',
+    date: new Date('2023-01-01'),
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam nisl tincidunt nunc, quis aliquet nisl nunc quis nisl. Nulla euismod, nisl eget ultricies aliquam, quam nisl tincidunt nunc, quis aliquet nisl nunc quis nisl.',
+  },
+  {
+    imgPath: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+    name: 'Clark Mante',
+    position: 'Technician',
+    date: new Date(),
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam nisl tincidunt nunc, quis aliquet nisl nunc quis nisl. Nulla euismod, nisl eget ultricies aliquam, quam nisl tincidunt nunc, quis aliquet nisl nunc quis nisl.',
+  },
+  {
+    imgPath: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+    name: 'Clark Mante',
+    position: 'Technician',
+    date: new Date('2022-10-21'),
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam nisl tincidunt nunc, quis aliquet nisl nunc quis nisl. Nulla euismod, nisl eget ultricies aliquam, quam nisl tincidunt nunc, quis aliquet nisl nunc quis nisl.',
+  },
+  {
+    imgPath: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+    name: 'Clark Mante',
+    position: 'Technician',
+    date: new Date('2023-2-28'),
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam nisl tincidunt nunc, quis aliquet nisl nunc quis nisl. Nulla euismod, nisl eget ultricies aliquam, quam nisl tincidunt nunc, quis aliquet nisl nunc quis nisl.',
+  },
+];
 
 const Posts: FC = () => {
-
   return (
-    <div className={styles.posts}>
+    <div className={styles.box}>
       <Scroll>
-        <AddPost />
-      </ Scroll>
+        <div className={styles.posts}>
+          <AddPost />
+          {posts.map((post, index) => (
+            <ShowPost
+              key={index}
+              imgPath={post.imgPath}
+              name={post.name}
+              position={post.position}
+              date={post.date}
+              content={post.content}
+            />
+          ))}
+        </div>
+      </Scroll>
     </div>
   );
 };
---
+++ client/src/pages/UserProfile/Posts/ShowPost/ShowPost.tsx
@@ -0,0 +1,59 @@
+import { FC } from 'react';
+
+import CommentBtn from '../../../../common/components/Comment/CommentBtn';
+import LikeBtn from '../../../../common/components/Like/LikeBtn';
+import RepostBtn from '../../../../common/components/Repost/RepostBtn';
+
+import styles from './ShowPost.module.scss';
+
+export interface ShowPostProps {
+  imgPath?: string;
+  name: string;
+  position: string;
+  date: Date;
+  content: string;
+}
+
+const ShowPost: FC<ShowPostProps> = ({ imgPath, name, position, date, content }) => {
+  const leaveComment = () => console.log('comment');
+  const repostPost = () => console.log('repost');
+  const likePost = () => console.log('like');
+
+  function formatDate(date: Date): string {
+    const currentDate = new Date();
+    const diffInMilliseconds = currentDate.getTime() - date.getTime() + 100000;
+
+    const millisecondsIn24Hours = 24 * 60 * 60 * 1000;
+
+    if (diffInMilliseconds < millisecondsIn24Hours) {
+      const options: Intl.DateTimeFormatOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
+      return date.toLocaleTimeString(undefined, options);
+    } else {
+      const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
+      return date.toLocaleDateString(undefined, options);
+    }
+  }
+
+  return (
+    <div className={styles.box}>
+      <div className={styles.userInfo}>
+        <img src={imgPath} alt="connection img" className={styles.image} />
+        <div className={styles.info}>
+          <div className={styles.name}>{name}</div>
+          <div className={styles.position}>{position}</div>
+          <div className={styles.date}>{formatDate(date)}</div>
+        </div>
+      </div>
+      <div className={styles.content}>{content}</div>
+      <div className={styles.reactions}>
+        <div className={styles.repostComment}>
+          <CommentBtn onClick={leaveComment} />
+          <RepostBtn onClick={repostPost} />
+        </div>
+        <LikeBtn onClick={likePost} />
+      </div>
+    </div>
+  );
+};
+
+export default ShowPost;
