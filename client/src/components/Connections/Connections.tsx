a403308f9285a732b9e94831a8324001c9a8fa6a
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-08-18 16:58:02 -0600

    pointer-view all

--- client/src/components/Connections/Connections.tsx
+++ client/src/components/Connections/Connections.tsx
@@ -20,6 +20,10 @@ const connectionList: connectionProps[] = [
   { id: 2, imgPath: defaultProfileImage, name: 'Adam Kenedi', position: 'Developer' },
   { id: 3, imgPath: defaultProfileImage, name: 'Hana Allen', position: 'Designer' },
   { id: 4, imgPath: defaultProfileImage, name: 'Geoffrey Donnelly', position: 'Developer' },
+  { id: 5, imgPath: defaultProfileImage, name: 'Ivanov Ivan', position: 'Manager' },
+  { id: 6, imgPath: defaultProfileImage, name: 'Tom Jerry', position: 'Testing' },
+  { id: 7, imgPath: defaultProfileImage, name: 'Alen Ford', position: 'Developer' },
+  { id: 8, imgPath: defaultProfileImage, name: 'Mark Smith', position: 'Developer' },
 ];
 
 const Connections: FC = () => {
@@ -27,15 +31,17 @@ const Connections: FC = () => {
     <SideBlock title="Connections" linkTo="/connections">
       <div className={styles.connections}>
         {connectionList &&
-          connectionList.map((connection) => (
-            <Connection
-              key={connection.id}
-              id={connection.id}
-              imgPath={connection.imgPath ? connection.imgPath : defaultProfileImage}
-              name={connection.name}
-              position={connection.position}
-            />
-          ))}
+          connectionList
+            .slice(0, 4)
+            .map((connection) => (
+              <Connection
+                key={connection.id}
+                id={connection.id}
+                imgPath={connection.imgPath ? connection.imgPath : defaultProfileImage}
+                name={connection.name}
+                position={connection.position}
+              />
+            ))}
       </div>
     </SideBlock>
   );
