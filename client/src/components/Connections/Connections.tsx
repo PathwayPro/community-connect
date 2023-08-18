322221fda3a3e274dea790d545d9da3278ecd4e0
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-08-18 16:58:02 -0600

    pointer-view all

--- client/src/components/Connections/Connections.tsx
+++ client/src/components/Connections/Connections.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Connection from './Connection/Connection';
 
@@ -17,26 +18,45 @@ const connectionList: connectionProps[] = [
   { id: 1, imgPath: defaultProfileImage, name: 'Clark Mante', position: 'Technician' },
   { id: 2, imgPath: defaultProfileImage, name: 'Adam Kenedi', position: 'Developer' },
   { id: 3, imgPath: '', name: 'Hana Allen', position: 'Designer' },
+  { id: 4, imgPath: '', name: 'Hana Allen', position: 'Designer' },
+  { id: 5, imgPath: '', name: 'Hana Allen', position: 'Designer' },
+  { id: 6, imgPath: '', name: 'Hana Allen', position: 'Designer' },
+  { id: 7, imgPath: '', name: 'Hana Allen', position: 'Designer' },
 ];
 
 const Connections: FC = () => {
+
+  const navigate = useNavigate();
+  const [showConnections, setShowConnections] = useState<number>(0);
+
+  const handleViewAllClick = () => {
+    navigate('/', { state: { connections: connectionList } });
+  };
+
+  useEffect(() => {
+    const maxConnections = Math.min(3, connectionList.length);
+    setShowConnections(maxConnections);
+  }, []);
+
+
   return (
     <div className={styles.box}>
       <div className={styles.title}>Connections</div>
       <div className={styles.connections}>
-        {connectionList &&
-          connectionList.map((connection) => (
-            <Connection
-              key={connection.id}
-              imgPath={connection.imgPath ? connection.imgPath : defaultProfileImage}
-              name={connection.name}
-              position={connection.position}
-            />
-          ))}
+        {connectionList.slice(0, showConnections).map((connection) => (
+          <Connection
+            key={connection.id}
+            imgPath={connection.imgPath ? connection.imgPath : defaultProfileImage}
+            name={connection.name}
+            position={connection.position}
+          />
+        ))}
       </div>
-      <div className={styles.seeMore}>
-        <a href="/">View all</a>
-      </div>
+      {showConnections < connectionList.length && (
+        <div className={styles.seeMore}>
+          <div onClick={handleViewAllClick}>View all</div>
+        </div>
+      )}
     </div>
   );
 };
