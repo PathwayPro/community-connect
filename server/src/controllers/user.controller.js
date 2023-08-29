7b24e11efb9b9390cde49aa15e29a5d3e646d07c
Tania Marokina <tania.marokina@gmail.com>
2023-08-29 02:34:18 -0600

    updated mentorship endpoints, services

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,5 +1,4 @@
 const httpStatus = require('http-status');
-const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { userService, userRoleService } = require('../services');
@@ -10,10 +9,9 @@ const createUser = catchAsync(async (req, res) => {
 });
 
 const getUsers = catchAsync(async (req, res) => {
-  const filter = pick(req.query, ['name', 'role']);
-  const options = pick(req.query, ['sortBy', 'limit', 'page']);
-  const result = await userService.queryUsers(filter, options);
-  res.send(result);
+  const filter = JSON.parse(req.query.filter);
+  const result = await userService.queryUsers(filter);
+  res.send(result.rows);
 });
 
 const getUser = catchAsync(async (req, res) => {
