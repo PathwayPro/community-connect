ac8a9450860e7adfc03601fede7281fe8245943d
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-11-01 15:48:07 -0600

    RegisterForm update. Checkbox input shows a warning message

--- client/src/components/RegisterForm/RegisterForm.tsx
+++ client/src/components/RegisterForm/RegisterForm.tsx
@@ -18,6 +18,7 @@ import {
   PASS_REGEX,
   ERROR_MESSAGE_PASSWORD,
   ERROR_MESSAGE_REPASSWORD,
+  ERROR_MESSAGE_TERMS_AND_CONDITIONS,
 } from '../../common/utils/formComponentsUtils';
 
 import styles from './RegisterForm.module.scss';
@@ -43,7 +44,8 @@ const RegisterForm: FC = () => {
     getValues,
     handleSubmit,
     control,
-    formState: { errors, isValid, isDirty },
+    // formState: { errors, isValid, isDirty },
+    formState: { errors },
   } = useForm<IFormInput>({ mode: 'onChange' });
 
   const firstName = register('firstName', {
@@ -84,6 +86,19 @@ const RegisterForm: FC = () => {
       return value === getValues('password') || ERROR_MESSAGE_REPASSWORD;
     },
   });
+
+  // const agreement = register('agreement', {
+  //   required: 'Please accept Terms and Conditions.',
+  //   validate: (value) => {
+  //     if (!value) {
+  //       return 'Please accept Terms and Conditions.';
+  //     }
+  //     return true; // Return true if validation passes
+  //   },
+  // });
+  // console.log('agreement', agreement);
+
+  // console.log(' formState: { errors }', errors);
 
   const [registerUser] = useRegisterUserMutation();
 
@@ -129,7 +144,7 @@ const RegisterForm: FC = () => {
         <Input
           name={firstName.name}
           id={`${formId}-${firstName.name}`}
-          label="First name *"
+          label="First name"
           autoComplete="on"
           className={styles.formField}
           onChange={firstName.onChange}
@@ -140,7 +155,7 @@ const RegisterForm: FC = () => {
         <Input
           name={lastName.name}
           id={`${formId}-${lastName.name}`}
-          label="Last name *"
+          label="Last name"
           autoComplete="on"
           className={styles.formField}
           onChange={lastName.onChange}
@@ -153,7 +168,7 @@ const RegisterForm: FC = () => {
         <Input
           name={email.name}
           id={`${formId}-${email.name}`}
-          label="Email *"
+          label="Email"
           type="email"
           autoComplete="on"
           className={styles.formField}
@@ -167,7 +182,7 @@ const RegisterForm: FC = () => {
         <Input
           name={password.name}
           id={`${formId}-${password.name}`}
-          label="Password *"
+          label="Password"
           type="password"
           isPassword={true}
           className={classNames(
@@ -185,7 +200,7 @@ const RegisterForm: FC = () => {
         <Input
           name={rePassword.name}
           id={`${formId}-${rePassword.name}`}
-          label="Re-enter Password *"
+          label="Re-enter Password"
           type="password"
           isPassword={true}
           className={styles.formField}
@@ -198,7 +213,7 @@ const RegisterForm: FC = () => {
 
       <div className={classNames(styles.formRow)}>
         <fieldset className={styles.checkboxField}>
-          <Controller
+          {/* <Controller
             name="agreement"
             control={control}
             rules={{
@@ -212,7 +227,29 @@ const RegisterForm: FC = () => {
                 className={classNames(styles.checkbox, fieldState.error && styles.error)}
               />
             )}
+          /> */}
+          <Controller
+            name="agreement"
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field, fieldState }) => (
+              <input
+                type="checkbox"
+                id={`${formId}-agreement`}
+                {...field}
+                className={classNames(styles.checkbox, fieldState.error && styles.error)}
+                onChange={(e) => {
+                  field.onChange(e);
+                  // setIsAgreementChecked(true);
+                  // setIsAgreementChecked(e.target.checked); // Track checkbox state
+                }}
+              />
+            )}
           />
+
+          {/*  */}
           <label htmlFor={`${formId}-agreement`} className={styles.checkboxLabel}>
             <span>You&nbsp;agree&nbsp;to&nbsp;our&nbsp;</span>
             <a href="#" className={styles.checkboxLabelLink}>
@@ -230,14 +267,17 @@ const RegisterForm: FC = () => {
         </fieldset>
       </div>
 
+      {/*  */}
+      {!getValues('agreement') && errors.agreement && (
+        <div className={styles.agreementErrorMessage}>
+          {ERROR_MESSAGE_TERMS_AND_CONDITIONS}
+          {/* {errors.agreement.message} */}
+        </div>
+      )}
+      {/*  */}
+
       <div className={styles.formButton}>
-        <Button
-          label="Register"
-          isSubmit
-          isDisabled={!isValid || !isDirty}
-          onClick={handleSubmit(onSubmit)}
-          size="small"
-        />
+        <Button label="Register" isSubmit onClick={handleSubmit(onSubmit)} size="small" />
       </div>
       <p className={styles.formBottomText}>
         <span>Already have an account? </span>
@@ -257,3 +297,263 @@ const RegisterForm: FC = () => {
 };
 
 export default RegisterForm;
+
+// import classNames from 'classnames';
+// import { FC, useState } from 'react';
+// import { Controller, useForm, SubmitHandler } from 'react-hook-form';
+
+// import { useAppDispatch } from '../../app/hooks';
+// import { useRegisterUserMutation } from '../../app/slices/apiSlice';
+// import { setCredentials } from '../../app/slices/authSlice';
+// import { showModal, MODAL_TYPE } from '../../app/slices/modalSlice';
+// import Button from '../../common/components/Button/Button';
+// import Heading from '../../common/components/Heading/Heading';
+// import Input from '../../common/components/Input/Input';
+// import { ERROR_MESSAGES } from '../../common/utils/errors';
+// import {
+//   NAME_REGEX,
+//   ERROR_MESSAGE_NAME,
+//   EMAIL_REGEX,
+//   ERROR_MESSAGE_EMAIL,
+//   PASS_REGEX,
+//   ERROR_MESSAGE_PASSWORD,
+//   ERROR_MESSAGE_REPASSWORD,
+// } from '../../common/utils/formComponentsUtils';
+
+// import styles from './RegisterForm.module.scss';
+
+// interface IFormInput {
+//   firstName: string;
+//   lastName: string;
+//   email: string;
+//   password: string;
+//   rePassword: string;
+//   agreement: string;
+// }
+
+// const formId = 'register';
+
+// const RegisterForm: FC = () => {
+//   const dispatch = useAppDispatch();
+//   const [emailError, setEmailError] = useState('');
+//   const [commonError, setCommonError] = useState('');
+
+//   const {
+//     register,
+//     getValues,
+//     handleSubmit,
+//     control,
+//     formState: { errors, isValid, isDirty },
+//   } = useForm<IFormInput>({ mode: 'onChange' });
+
+//   const firstName = register('firstName', {
+//     required: 'First name is required',
+//     pattern: {
+//       value: NAME_REGEX,
+//       message: ERROR_MESSAGE_NAME,
+//     },
+//   });
+
+//   const lastName = register('lastName', {
+//     required: 'Last name is required',
+//     pattern: {
+//       value: NAME_REGEX,
+//       message: ERROR_MESSAGE_NAME,
+//     },
+//   });
+
+//   const email = register('email', {
+//     required: 'Email is required',
+//     pattern: {
+//       value: EMAIL_REGEX,
+//       message: ERROR_MESSAGE_EMAIL,
+//     },
+//   });
+
+//   const password = register('password', {
+//     required: 'Password is required',
+//     pattern: {
+//       value: PASS_REGEX,
+//       message: ERROR_MESSAGE_PASSWORD,
+//     },
+//   });
+
+//   const rePassword = register('rePassword', {
+//     required: 'Re-enter password is required',
+//     validate: (value) => {
+//       return value === getValues('password') || ERROR_MESSAGE_REPASSWORD;
+//     },
+//   });
+
+//   const [registerUser] = useRegisterUserMutation();
+
+//   const onSubmit: SubmitHandler<IFormInput> = async (values) => {
+//     const bodyObject = {
+//       firstName: values.firstName,
+//       lastName: values.lastName,
+//       email: values.email,
+//       password: values.password,
+//     };
+
+//     await registerUser(bodyObject)
+//       .unwrap()
+//       .then((data) => {
+//         setCommonError('');
+//         dispatch(setCredentials(data));
+//         dispatch(showModal({ content: MODAL_TYPE.SEND_CONFIRMATION_EMAIL }));
+//       })
+//       .catch((error) => {
+//         if (error?.data?.message === ERROR_MESSAGES.EMAIL_TAKEN) {
+//           setCommonError('');
+//           setEmailError(ERROR_MESSAGES.EMAIL_TAKEN);
+//         } else {
+//           // Unhandled errors
+//           setCommonError(ERROR_MESSAGES.SERVER_ERROR);
+//           setEmailError('');
+//         }
+//       });
+//   };
+
+//   const onChange = (event: React.ChangeEvent<HTMLInputElement>, onChangeHandler: React.ChangeEventHandler) => {
+//     setEmailError('');
+//     onChangeHandler(event);
+//   };
+
+//   return (
+//     <form className={styles.form}>
+//       <Heading tagType="h5" className={styles.formTitle}>
+//         Create a new account
+//       </Heading>
+//       {commonError && <p className={styles.errorMessage}>{ERROR_MESSAGES.SERVER_ERROR}</p>}
+//       <div className={styles.formRow}>
+//         <Input
+//           name={firstName.name}
+//           id={`${formId}-${firstName.name}`}
+//           label="First name *"
+//           autoComplete="on"
+//           className={styles.formField}
+//           onChange={firstName.onChange}
+//           onBlur={firstName.onBlur}
+//           ref={firstName.ref}
+//           errorMessage={errors.firstName?.message}
+//         />
+//         <Input
+//           name={lastName.name}
+//           id={`${formId}-${lastName.name}`}
+//           label="Last name *"
+//           autoComplete="on"
+//           className={styles.formField}
+//           onChange={lastName.onChange}
+//           onBlur={lastName.onBlur}
+//           ref={lastName.ref}
+//           errorMessage={errors.lastName?.message}
+//         />
+//       </div>
+//       <div className={styles.formRow}>
+//         <Input
+//           name={email.name}
+//           id={`${formId}-${email.name}`}
+//           label="Email *"
+//           type="email"
+//           autoComplete="on"
+//           className={styles.formField}
+//           onChange={(event) => onChange(event, email.onChange)}
+//           onBlur={email.onBlur}
+//           ref={email.ref}
+//           errorMessage={errors.email?.message || emailError}
+//         />
+//       </div>
+//       <div className={classNames(styles.formRow)}>
+//         <Input
+//           name={password.name}
+//           id={`${formId}-${password.name}`}
+//           label="Password *"
+//           type="password"
+//           isPassword={true}
+//           className={classNames(
+//             styles.formField,
+//             errors.password &&
+//               errors.password?.message &&
+//               errors.password?.message?.length > 20 &&
+//               styles.formFieldPassword
+//           )}
+//           onChange={password.onChange}
+//           onBlur={password.onBlur}
+//           ref={password.ref}
+//           errorMessage={errors.password?.message}
+//         />
+//         <Input
+//           name={rePassword.name}
+//           id={`${formId}-${rePassword.name}`}
+//           label="Re-enter Password *"
+//           type="password"
+//           isPassword={true}
+//           className={styles.formField}
+//           onChange={rePassword.onChange}
+//           onBlur={rePassword.onBlur}
+//           ref={rePassword.ref}
+//           errorMessage={errors.rePassword?.message}
+//         />
+//       </div>
+
+//       <div className={classNames(styles.formRow)}>
+//         <fieldset className={styles.checkboxField}>
+//           <Controller
+//             name="agreement"
+//             control={control}
+//             rules={{
+//               required: true,
+//             }}
+//             render={({ field, fieldState }) => (
+//               <input
+//                 type="checkbox"
+//                 id={`${formId}-agreement`}
+//                 {...field}
+//                 className={classNames(styles.checkbox, fieldState.error && styles.error)}
+//               />
+//             )}
+//           />
+//           <label htmlFor={`${formId}-agreement`} className={styles.checkboxLabel}>
+//             <span>You&nbsp;agree&nbsp;to&nbsp;our&nbsp;</span>
+//             <a href="#" className={styles.checkboxLabelLink}>
+//               Terms and Conditions
+//             </a>
+//             {', '}
+//             <a href="#" className={styles.checkboxLabelLink}>
+//               Privacy Statement
+//             </a>
+//             {', and '}
+//             <a href="#" className={styles.checkboxLabelLink}>
+//               Security Policy
+//             </a>
+//           </label>
+//         </fieldset>
+//       </div>
+
+//       <div className={styles.formButton}>
+//         <Button
+//           label="Register"
+//           isSubmit
+//           isDisabled={!isValid || !isDirty}
+//           onClick={handleSubmit(onSubmit)}
+//           size="small"
+//         />
+//       </div>
+//       <p className={styles.formBottomText}>
+//         <span>Already have an account? </span>
+//         <a
+//           href=""
+//           className={styles.formBottomLink}
+//           onClick={(e) => {
+//             e.preventDefault();
+//             dispatch(showModal({ content: MODAL_TYPE.LOGIN }));
+//           }}
+//         >
+//           Log&nbsp;in&nbsp;now
+//         </a>
+//       </p>
+//     </form>
+//   );
+// };
+
+// export default RegisterForm;
