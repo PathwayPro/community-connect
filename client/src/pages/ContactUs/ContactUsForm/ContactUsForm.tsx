fb6a6d0d827c1ac179be5fcaad7a0ef07a1362a3
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-11-14 17:56:40 -0700

    Add more styles

--- client/src/pages/ContactUs/ContactUsForm/ContactUsForm.tsx
+++ client/src/pages/ContactUs/ContactUsForm/ContactUsForm.tsx
@@ -9,22 +9,12 @@ const ContactUsForm = () => {
       <div className={styles.formRow}>
         <Input label="First name" className={styles.input} />
         <Input label="Last name" className={styles.input} />
-        {/* <input className={styles.input} /> */}
       </div>
 
       <div className={styles.formRow}>
         <Input label="Email" className={styles.input} />
         <Input label="Reason (optional)" className={styles.input} />
       </div>
-
-      {/* <div className={styles.formRow}>
-        <Input label="Message" className={styles.input} />
-      </div> */}
-
-      {/* <div className={`${styles.formRow} ${styles.textarea}`}>
-        <label htmlFor="message">Message</label>
-        <textarea id="message" name="message" />
-      </div> */}
 
       <label htmlFor="message" className={styles.label}>
         Message
