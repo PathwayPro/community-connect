405af1831fbbcc231c5b5ba75c727a05eef2bb5e
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-11-30 20:51:03 -0700

    Work on custom pagination

--- admin/src/Pages/MentorsList.tsx
+++ admin/src/Pages/MentorsList.tsx
@@ -1,16 +1,41 @@
-import { List, Datagrid, TextField, EmailField, FunctionField, ShowButton, ReferenceField } from 'react-admin';
+import React from 'react';
+import {
+  List,
+  Datagrid,
+  TextField,
+  EmailField,
+  FunctionField,
+  ShowButton,
+  ReferenceField,
+  Pagination,
+} from 'react-admin';
 
 import styles from '../App.module.scss';
 import { ListHeading } from '../common/ListHeading/ListHeading';
 
+interface CustomPaginationProps {
+  page: number;
+  perPage: number;
+  setPage?: () => void;
+  setPerPage?: () => void;
+  total: number;
+}
+
+const CustomPagination: React.FC<CustomPaginationProps> = ({ page, perPage, total, ...rest }) => {
+  const totalPages = Math.ceil(total / perPage) || 1;
+
+  return <Pagination rowsPerPageOptions={[11]} page={page} count={totalPages} {...rest} />;
+};
+
 export const MentorsList = () => {
   return (
     <>
-      <List className={styles.list} exporter={false}>
+      <List className={styles.list} exporter={false} pagination={<CustomPagination />}>
         <div className={styles.greet}>Hello Evano 👋🏼,</div>
         <ListHeading listName="Mentors" />
 
         <Datagrid rowClick="show" bulkActionButtons={false} className={styles.listRow}>
+          {/* Your Datagrid fields here */}
           <ReferenceField label="Full name" source="userId" reference="users">
             <FunctionField render={(record: Record<string, string>) => `${record.firstName} ${record.lastName}`} />
           </ReferenceField>
@@ -27,3 +52,33 @@ export const MentorsList = () => {
     </>
   );
 };
+
+// import { List, Datagrid, TextField, EmailField, FunctionField, ShowButton, ReferenceField } from 'react-admin';
+
+// import styles from '../App.module.scss';
+// import { ListHeading } from '../common/ListHeading/ListHeading';
+
+// export const MentorsList = () => {
+//   return (
+//     <>
+//       <List className={styles.list} exporter={false}>
+//         <div className={styles.greet}>Hello Evano 👋🏼,</div>
+//         <ListHeading listName="Mentors" />
+
+//         <Datagrid rowClick="show" bulkActionButtons={false} className={styles.listRow}>
+//           <ReferenceField label="Full name" source="userId" reference="users">
+//             <FunctionField render={(record: Record<string, string>) => `${record.firstName} ${record.lastName}`} />
+//           </ReferenceField>
+//           <ReferenceField label="Email" source="userId" reference="users">
+//             <EmailField source="email" />
+//           </ReferenceField>
+//           <TextField source="fieldOfExpertise" />
+//           <TextField source="yearsOfExperience" />
+//           <TextField source="status" />
+//           <ShowButton />
+//         </Datagrid>
+//         <div className={styles.listFooter}>Showing data 1 to 12 of 256K entries</div>
+//       </List>
+//     </>
+//   );
+// };
