a13dc0fc45b8a34c85ff6bbd37d96eced840b049
erfan1995 <aliriza.erfan@gmail.com>
2024-03-17 11:52:21 -0600

    feat:post update & delete swagger

--- server/src/routes/v1/post.route.js
+++ server/src/routes/v1/post.route.js
@@ -22,3 +22,107 @@ router.route('/:postId').delete(auth(), postController.deletePost);
 router.route('/:postId').put(auth(), validate(postValidation.createPost), postController.updatePost);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Posts
+ *   description: User management and retrieval
+ */
+
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Create a post
+ *     description: Each verified user create other posts.
+ *     tags: [Posts]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - content
+ *             properties:
+ *               content:
+ *                 type: text
+ *             example:
+ *               content: This is my first post!
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Post'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *
+ *   patch:
+ *     summary: Update a post
+ *     description: Logged in users can  update their own posts.
+ *     tags: [Posts]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Post id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               content:
+ *                 type: text
+ *             example:
+ *               content: This is my post updated content!
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Post'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *
+ *   delete:
+ *     summary: Delete a post
+ *     description: Logged in users can delete only their posts.
+ *     tags: [Posts]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Post id
+ *     responses:
+ *       "200":
+ *         description: No content
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
